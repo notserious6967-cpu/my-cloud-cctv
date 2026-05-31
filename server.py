bbdb96b12d841136518d188834086fb7920e82fa
notserious6967-cpu <notserious6967@gmail.com>
2026-05-31 19:16:04 +0530

    Migrated from deprecated eventlet to stable gevent websocket stack

--- server.py
+++ server.py
@@ -56,8 +56,8 @@ def on_heist_alarm(data):
 
 
     if __name__ == '__main__':
-        import eventlet
-        eventlet.monkey_patch()
+        from gevent import monkey
+        monkey.patch_all()
         port = int(os.environ.get('PORT', 5000))
         socketio.run(app, host='0.0.0.0', port=port, debug=False)
 
